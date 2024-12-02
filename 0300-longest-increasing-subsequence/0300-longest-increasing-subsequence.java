e149ba93b583a63fe1c869e860784e37f1039df0
Vidhan Arya <vidhanarya50@gmail.com>
2024-12-02 12:18:21 +0530

    Time: 6 ms (88.5%), Space: 44.1 MB (57.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int ans = 0;
+        List<Integer> seq = new ArrayList<>();
+        seq.add(nums[0]);
 
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] >= nums[i]) continue;
-                dp[i] = Math.max(dp[i], dp[j] + 1);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > seq.get(seq.size() - 1)) seq.add(nums[i]);
+            else {
+                int j = binarySearch(seq, nums[i]);
+                seq.set(j, nums[i]);
             }
-            ans = Math.max(ans, dp[i]);
         }
 
-        return ans;
+        return seq.size();
+    }
+
+    public int binarySearch(List<Integer> seq, int target) {
+        int left = 0, right = seq.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (seq.get(mid) == target) return mid;
+            else if (seq.get(mid) > target) right = mid;
+            else left = mid + 1;
+        }
+
+        return left;
     }
 }
