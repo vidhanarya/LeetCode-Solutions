459cc48d55a517b026b80edb85ceaa93abc240ba
Vidhan Arya <vidhanarya50@gmail.com>
2024-12-07 23:42:31 +0530

    Time: 0 ms (100%), Space: 45.7 MB (15.87%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,16 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int loner = 0;
-
-        for (int i = 0; i < 32; i++) {
-            int lonerBit = 0;
-            for (int n: nums) {
-                lonerBit += ((n & (1 << i)) != 0) ? 1 : 0;
-            }
-            lonerBit = lonerBit % 3;
-            loner = loner | (lonerBit << i);
+        int seenOnce = 0, seenTwice = 0;
+        for (int n: nums) {
+            seenOnce = (seenOnce ^ n) & (~seenTwice);
+            seenTwice = (seenTwice ^ n) & (~seenOnce);
         }
 
-        return loner;
+        return seenOnce;
     }
 }
