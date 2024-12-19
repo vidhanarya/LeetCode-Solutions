1c83e4865ec9b654061edfd50edc080aa7636edd
Vidhan Arya <vidhanarya50@gmail.com>
2024-12-19 16:09:23 +0530

    Time: 0 ms (100%), Space: 40.7 MB (74.49%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,17 +1,11 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
         int n = arr.length;
-        int[] minPrefix = new int[n];
-        
-        minPrefix[n-1] = arr[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            minPrefix[i] = Math.min(minPrefix[i+1], arr[i]);
-        }
+        int currMax = arr[0], chunks = 0;
 
-        int currMax = arr[0], chunks = 1;
-        for (int i = 1; i < n; i++) {
-            if (currMax <= minPrefix[i]) chunks++;
+        for (int i = 0; i < n; i++) {
             currMax = Math.max(arr[i], currMax);
+            if (currMax == i) chunks++;
         }
 
         return chunks;
