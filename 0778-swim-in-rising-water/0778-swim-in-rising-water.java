779a97d7627fea7a623dc05f9630094473aaae14
Vidhan Arya <vidhanarya50@gmail.com>
2025-10-06 11:42:28 +0530

    Time: 252 ms (5.02%), Space: 45.6 MB (6.48%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -11,38 +11,52 @@ class State {
 }
 
 class Solution {
+    int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int[][] visited = new int[n][n];
-        PriorityQueue<State> bfs = new PriorityQueue<>(Comparator.comparingInt(a -> a.t));
+        int left = 0, right = Integer.MAX_VALUE;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
 
-        bfs.add(new State(0, 0, grid[0][0]));
-        visited[0][0] = grid[0][0];
-        while (!bfs.isEmpty()) {
-            State state = bfs.poll();
-            if (state.x == n-1 && state.y == n-1) {
-                return state.t;
-            }
+            if (canSwimWithinTime(grid, n, mid)) right = mid;
+            else left = mid + 1;
+        }
 
-            for (int[] direction : directions) {
-                int newX = state.x + direction[0];
-                int newY = state.y + direction[1];
+        return left;
+    }
 
-                if (!isValid(n, newX, newY)) continue;
+    public boolean canSwimWithinTime(int[][] grid, int n, int time) {
+        if (grid[0][0] > time) return false;
 
-                int newT = Math.max(state.t, grid[newX][newY]);
-                if (visited[newX][newY] != 0 && visited[newX][newY] <= newT) continue;
+        Stack<State> dfs = new Stack<>();
+        Set<String> visited = new HashSet<>();
+        dfs.push(new State(0, 0, grid[0][0]));
+        visited.add(getEdge(0, 0));
 
-                bfs.add(new State(newX, newY, newT));
-                visited[newX][newY] = newT;
+        while (!dfs.isEmpty()) {
+            State state = dfs.pop();
+            if (state.x == n - 1 && state.y == n - 1) return true;
+
+            for (int[] direction: directions) {
+                int x = state.x + direction[0];
+                int y = state.y + direction[1];
+                if (visited.contains(getEdge(x, y)) || !valid(n, x, y)) continue;
+                if (grid[x][y] > time) continue;
+
+                dfs.push(new State(x, y, Math.max(state.t, grid[x][y])));
+                visited.add(getEdge(x, y));
             }
         }
 
-        return 2499;
+        return false;
     }
 
-    private boolean isValid(int n, int x, int y) {
+    public boolean valid(int n, int x, int y) {
         return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+
+    public String getEdge(int x, int y) {
+        return String.valueOf(x) + "-" + String.valueOf(y);
     }
 }
